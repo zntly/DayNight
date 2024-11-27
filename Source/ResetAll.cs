461e61b16a4837e29edb4d4c9f1d752ff9e204d0
zntly <83675710+zntly@users.noreply.github.com>
2024-11-26 23:45:44 -0500

    Add files via upload

---
+++ Source/ResetAll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cinematics.Players;
+using HarmonyLib;
+using Server.Shared.Cinematics.Data;
+
+namespace SkyControllerPP
+{
+	// Token: 0x0200001A RID: 26
+	[HarmonyPatch(typeof(FactionWinsStandardCinematicPlayer), "Cleanup")]
+	internal class ResetAll
+	{
+		// Token: 0x0600009C RID: 156
+		[HarmonyPrefix]
+		public static void Prefix(ICinematicData cinematicData)
+		{
+			SkyInfo.Instance.Pest = false;
+			SkyInfo.Instance.War = false;
+			SkyInfo.Instance.Famine = false;
+			SkyInfo.Instance.Death = false;
+			SkyInfo.Phase = "Day";
+			SkyInfo.Instance.UpdateSky();
+		}
+	}
+}
