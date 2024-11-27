a05f5d299f0d6a2d4a5c083a72f18e85e74e18a4
zntly <83675710+zntly@users.noreply.github.com>
2024-11-26 23:42:35 -0500

    Add files via upload

---
+++ Source/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BetterTOS2;
+using BetterTOS2.Observations;
+using SML;
+
+namespace SkyControllerPP
+{
+	// Token: 0x0200001B RID: 27
+	public static class Utils
+	{
+		// Token: 0x0600009D RID: 157 RVA: 0x0000208A File Offset: 0x0000028A
+		public static bool BTOS2Exists()
+		{
+			return ModStates.IsEnabled("curtis.tuba.better.tos2");
+		}
+
+		// Token: 0x0600009E RID: 158 RVA: 0x00005B58 File Offset: 0x00003D58
+		public static bool IsBTOS2()
+		{
+			bool result;
+			try
+			{
+				result = Utils.IsBTOS2Bypass();
+			}
+			catch
+			{
+				result = false;
+			}
+			return result;
+		}
+
+		// Token: 0x0600009F RID: 159 RVA: 0x000023C9 File Offset: 0x000005C9
+		private static bool IsBTOS2Bypass()
+		{
+			return Utils.BTOS2Exists() && BTOSInfo.IS_MODDED;
+		}
+
+		// Token: 0x060000A0 RID: 160 RVA: 0x000023D9 File Offset: 0x000005D9
+		public static bool CourtCheck()
+		{
+			return GameObservationsPatch.musicOverrideObservation.Data.court;
+		}
+	}
+}
