bba5aac6a3900849c53d080e324b0e641f560184
zntly <83675710+zntly@users.noreply.github.com>
2024-12-04 17:32:06 -0500

    Update ShaderOverrider.cs

--- Source/SkyControllerPP/ShaderOverrider.cs
+++ Source/SkyControllerPP/ShaderOverrider.cs
@@ -1,8 +1,6 @@
-﻿using System;
+using System;
 using HarmonyLib;
 using Server.Shared.State;
-using Services;
-using Sight;
 using UnityEngine;
 
 namespace SkyControllerPP
@@ -11,9 +9,9 @@ namespace SkyControllerPP
 	[HarmonyPatch(typeof(GlobalShaderColors), "ValidateColors")]
 	public class ShaderOverrider
 	{
-		// Token: 0x0600008E RID: 142
-		[HarmonyPrefix]
-		public static void Prefix(GlobalShaderColors __instance)
+		// Token: 0x06000092 RID: 146
+		[HarmonyPostfix]
+		public static void Postfix(GlobalShaderColors __instance)
 		{
 			if (Utils.IsBTOS2())
 			{
@@ -21,7 +19,6 @@ namespace SkyControllerPP
 				{
 					SkyInfo.Phase = "Court";
 				}
-				MapShaderColors mapShaderColors = Service.Game.Map.mapShaderColors;
 				SkyInfo.SkyType skyType = SkyInfo.GetCurrentPermSkyType();
 				if (SkyInfo.Phase != "Tribunal" && SkyInfo.Phase != "Court" && SkyInfo.Phase != "Daybreak" && (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War))
 				{
@@ -31,20 +28,18 @@ namespace SkyControllerPP
 				{
 					skyType = SkyInfo.GetSyncedSkyType();
 				}
-				if (mapShaderColors != null)
+				SkyInfo.Instance.SetCurrentSkyType(skyType);
+				if (__instance.colorProviders.Contains(GlobalShaderColors.ColorProviders.Cinematic) && __instance.cinematicPlayer.cinematicType == CinematicType.RoleReveal)
 				{
-					mapShaderColors.dayColor = SkyInfo.GetSkyColor(skyType);
-					mapShaderColors.nightColor = SkyInfo.GetSkyColor(skyType);
+					Color targetExteriorGlobalTintColor = __instance.targetExteriorGlobalTintColor;
+					__instance.targetExteriorGlobalTintColor = targetExteriorGlobalTintColor * SkyInfo.GetSkyColor(skyType);
 					return;
 				}
-				__instance.dayColor = SkyInfo.GetSkyColor(skyType);
-				__instance.nightColor = SkyInfo.GetSkyColor(skyType);
-				SkyInfo.Instance.SetCurrentSkyType(skyType);
+				__instance.targetExteriorGlobalTintColor = SkyInfo.GetSkyColor(skyType);
 				return;
 			}
 			else
 			{
-				MapShaderColors mapShaderColors2 = Service.Game.Map.mapShaderColors;
 				SkyInfo.SkyType skyType2 = SkyInfo.GetCurrentPermSkyType();
 				if (SkyInfo.Phase != "Tribunal" && (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War))
 				{
@@ -54,70 +49,15 @@ namespace SkyControllerPP
 				{
 					skyType2 = SkyInfo.GetSyncedSkyType();
 				}
-				if (mapShaderColors2 != null)
+				SkyInfo.Instance.SetCurrentSkyType(skyType2);
+				if (__instance.colorProviders.Contains(GlobalShaderColors.ColorProviders.Cinematic) && __instance.cinematicPlayer.cinematicType == CinematicType.RoleReveal)
 				{
-					mapShaderColors2.dayColor = SkyInfo.GetSkyColor(skyType2);
-					mapShaderColors2.nightColor = SkyInfo.GetSkyColor(skyType2);
+					Color targetExteriorGlobalTintColor2 = __instance.targetExteriorGlobalTintColor;
+					__instance.targetExteriorGlobalTintColor = targetExteriorGlobalTintColor2 * SkyInfo.GetSkyColor(skyType2);
 					return;
 				}
-				__instance.dayColor = SkyInfo.GetSkyColor(skyType2);
-				__instance.nightColor = SkyInfo.GetSkyColor(skyType2);
-				SkyInfo.Instance.SetCurrentSkyType(skyType2);
+				__instance.targetExteriorGlobalTintColor = SkyInfo.GetSkyColor(skyType2);
 				return;
-			}
-		}
-
-		// Token: 0x0600008F RID: 143
-		[HarmonyPostfix]
-		public static void Postfix(GlobalShaderColors __instance)
-		{
-			if (__instance.colorProviders.Contains(GlobalShaderColors.ColorProviders.Cinematic))
-			{
-				if (Utils.IsBTOS2())
-				{
-					SkyInfo.SkyType skyType = SkyInfo.GetCurrentPermSkyType();
-					if (SkyInfo.Phase != "Tribunal" && SkyInfo.Phase != "Court" && SkyInfo.Phase != "Daybreak" && (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War))
-					{
-						skyType = SkyInfo.GetCurrentApocSkyType();
-					}
-					if (skyType == SkyInfo.SkyType.None)
-					{
-						skyType = SkyInfo.GetSyncedSkyType();
-					}
-					if (__instance.cinematicPlayer.cinematicType != CinematicType.RoleReveal)
-					{
-						__instance.targetExteriorGlobalTintColor = SkyInfo.GetSkyColor(skyType);
-						return;
-					}
-					if (__instance.cinematicPlayer.cinematicType == CinematicType.RoleReveal)
-					{
-						Color targetExteriorGlobalTintColor = __instance.targetExteriorGlobalTintColor;
-						__instance.targetExteriorGlobalTintColor = targetExteriorGlobalTintColor * SkyInfo.GetSkyColor(skyType);
-						return;
-					}
-				}
-				else
-				{
-					SkyInfo.SkyType skyType2 = SkyInfo.GetCurrentPermSkyType();
-					if (SkyInfo.Phase != "Tribunal" && (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War))
-					{
-						skyType2 = SkyInfo.GetCurrentApocSkyType();
-					}
-					if (skyType2 == SkyInfo.SkyType.None)
-					{
-						skyType2 = SkyInfo.GetSyncedSkyType();
-					}
-					if (__instance.cinematicPlayer.cinematicType != CinematicType.RoleReveal)
-					{
-						__instance.targetExteriorGlobalTintColor = SkyInfo.GetSkyColor(skyType2);
-						return;
-					}
-					if (__instance.cinematicPlayer.cinematicType == CinematicType.RoleReveal)
-					{
-						Color targetExteriorGlobalTintColor2 = __instance.targetExteriorGlobalTintColor;
-						__instance.targetExteriorGlobalTintColor = targetExteriorGlobalTintColor2 * SkyInfo.GetSkyColor(skyType2);
-					}
-				}
 			}
 		}
 	}
