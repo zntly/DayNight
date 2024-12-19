46ea0822c2349cc050939d90a20af6dc06e707e5
zntly <83675710+zntly@users.noreply.github.com>
2024-12-18 22:09:42 -0500

    Added BTOS2 Main Menu Compatibility
    The sky will change to Tribunal/Court/Daybreak Skybox when the appropriate event is triggered by clicking on the BTOS2 logo in the main menu

---
+++ Source/SkyControllerPP/MyPluginInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SkyControllerPP
+{
+	// Token: 0x02000014 RID: 20
+	public static class MyPluginInfo
+	{
+		// Token: 0x0400005A RID: 90
+		public const string PLUGIN_GUID = "DayNight";
+
+		// Token: 0x0400005B RID: 91
+		public const string PLUGIN_NAME = "DayNight";
+
+		// Token: 0x0400005C RID: 92
+		public const string PLUGIN_VERSION = "1.0.0";
+	}
+}
---
+++ Source/SkyControllerPP/SkyboxControllerPatch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using HarmonyLib;
+using Home.Shared;
+
+namespace SkyControllerPP
+{
+	// Token: 0x0200000A RID: 10
+	[HarmonyPatch(typeof(SkyboxController), "Update")]
+	public class SkyboxControllerPatch
+	{
+		// Token: 0x0600004B RID: 75 RVA: 0x00004288 File Offset: 0x00002488
+		[HarmonyPostfix]
+		public static void Postfix(SkyboxController __instance)
+		{
+			if (SkyInfo.Already != __instance)
+			{
+				SkyInfo.Already = __instance;
+				if (Leo.IsGameScene())
+				{
+					SkyInfo.Instance = __instance.gameObject.AddComponent<SkyControllerPlus>();
+					SkyInfo.Phase = "Day";
+					return;
+				}
+				SkyInfo.Instance = __instance.gameObject.AddComponent<SkyControllerPlus>();
+				SkyInfo.Phase = "NotGame";
+				ApplicationController.ApplicationContext.StartCoroutine(SkyboxControllerPatch.SetSkyShader());
+			}
+		}
+
+		// Token: 0x0600004D RID: 77 RVA: 0x000020D4 File Offset: 0x000002D4
+		private static IEnumerator SetSkyShader()
+		{
+			yield return null;
+			if (Leo.IsHomeScene())
+			{
+				switch (SkyInfo.Instance.GetCurrentSkyType())
+				{
+				case SkyInfo.SkyType.Day:
+					SkyInfo.Instance.UpdateIntroClouds("Day");
+					break;
+				case SkyInfo.SkyType.Night:
+					SkyInfo.Instance.UpdateIntroClouds("Night");
+					break;
+				case SkyInfo.SkyType.Dawn:
+					SkyInfo.Instance.UpdateIntroClouds("Dawn");
+					break;
+				case SkyInfo.SkyType.Dusk:
+					SkyInfo.Instance.UpdateIntroClouds("Dawn");
+					break;
+				case SkyInfo.SkyType.BloodMoon:
+					SkyInfo.Instance.UpdateIntroClouds("BloodMoon");
+					break;
+				case SkyInfo.SkyType.Storm:
+					SkyInfo.Instance.UpdateIntroClouds("Storm");
+					break;
+				case SkyInfo.SkyType.Eclipse:
+					SkyInfo.Instance.UpdateIntroClouds("Invis");
+					break;
+				case SkyInfo.SkyType.Winter:
+					SkyInfo.Instance.UpdateIntroClouds("Invis");
+					break;
+				}
+			}
+			SkyInfo.Instance.UpdateSky();
+			yield break;
+		}
+
+		// Token: 0x04000024 RID: 36
+		public static bool Last;
+
+		// Token: 0x04000025 RID: 37
+		public static string LastScene;
+	}
+}
