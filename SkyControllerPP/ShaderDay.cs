bccc82198187cd59801367ffe090ca573959cd85
zntly <83675710+zntly@users.noreply.github.com>
2025-03-15 20:05:59 -0400

    Add files via upload

---
+++ SkyControllerPP/ShaderDay.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using HarmonyLib;
+using SML;
+using UnityEngine;
+
+namespace SkyControllerPP
+{
+	// Token: 0x0200000F RID: 15
+	[HarmonyPatch(typeof(GlobalShaderColors), "SetToDay")]
+	public class ShaderDay
+	{
+		// Token: 0x06000060 RID: 96
+		[HarmonyPrefix]
+		public static void Prefix(GlobalShaderColors __instance)
+		{
+			if (SkyInfo.Phase != "Tribunal" && SkyInfo.Phase != "Court" && SkyInfo.Phase != "Daybreak")
+			{
+				SkyInfo.Phase = "Day";
+				SkyInfo.SkyType skyType = SkyInfo.GetCurrentPermSkyType();
+				if (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War)
+				{
+					skyType = SkyInfo.GetCurrentApocSkyType();
+				}
+				if (skyType == SkyInfo.SkyType.None)
+				{
+					skyType = SkyInfo.GetSyncedSkyType();
+				}
+				if (SkyInfo.Instance && SkyInfo.CurrentActive == skyType && Main.Snowflakes != null)
+				{
+					if (ModSettings.GetBool("Color Snowflakes to Shader Color"))
+					{
+						if (Main.Snowflakes.startColor != SkyInfo.GetSkyColor(skyType))
+						{
+							Main.Snowflakes.startColor = SkyInfo.GetSkyColor(skyType);
+							Main.Snowflakes.Clear();
+							Main.Snowflakes.Emit(250);
+							return;
+						}
+					}
+					else if (Main.Snowflakes.startColor != Color.white)
+					{
+						Main.Snowflakes.startColor = Color.white;
+						Main.Snowflakes.Clear();
+						Main.Snowflakes.Emit(250);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SkyControllerPP/ShaderNight.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using HarmonyLib;
+using SML;
+using UnityEngine;
+
+namespace SkyControllerPP
+{
+	// Token: 0x02000010 RID: 16
+	[HarmonyPatch(typeof(GlobalShaderColors), "SetToNight")]
+	public class ShaderNight
+	{
+		// Token: 0x06000062 RID: 98
+		[HarmonyPrefix]
+		public static void Prefix(GlobalShaderColors __instance)
+		{
+			SkyInfo.Phase = "Night";
+			if (ModSettings.GetString("Jail Cell Shading Mode") == "Night")
+			{
+				SkyInfo.SkyType skyType = SkyInfo.GetCurrentPermSkyType();
+				if (SkyInfo.Phase != "Tribunal" && SkyInfo.Phase != "Court" && SkyInfo.Phase != "Daybreak" && (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War))
+				{
+					skyType = SkyInfo.GetCurrentApocSkyType();
+				}
+				if (skyType == SkyInfo.SkyType.None)
+				{
+					skyType = SkyInfo.GetSyncedSkyType();
+				}
+				Color skyColor = SkyInfo.GetSkyColor(skyType);
+				if (skyColor != JailCellShader.lastcolor)
+				{
+					JailCellShader.ShadeCell(skyColor);
+				}
+			}
+			SkyInfo.SkyType skyType2 = SkyInfo.GetCurrentPermSkyType();
+			if (SkyInfo.Instance.Pest || SkyInfo.Instance.Famine || SkyInfo.Instance.Death || SkyInfo.Instance.War)
+			{
+				skyType2 = SkyInfo.GetCurrentApocSkyType();
+			}
+			if (skyType2 == SkyInfo.SkyType.None)
+			{
+				skyType2 = SkyInfo.GetSyncedSkyType();
+			}
+			if (SkyInfo.Instance && SkyInfo.CurrentActive == skyType2 && Main.Snowflakes != null)
+			{
+				if (ModSettings.GetBool("Color Snowflakes to Shader Color"))
+				{
+					if (Main.Snowflakes.startColor != SkyInfo.GetSkyColor(skyType2))
+					{
+						Main.Snowflakes.startColor = SkyInfo.GetSkyColor(skyType2);
+						Main.Snowflakes.Clear();
+						Main.Snowflakes.Emit(250);
+						return;
+					}
+				}
+				else if (Main.Snowflakes.startColor != Color.white)
+				{
+					Main.Snowflakes.startColor = Color.white;
+					Main.Snowflakes.Clear();
+					Main.Snowflakes.Emit(250);
+				}
+			}
+		}
+	}
+}
